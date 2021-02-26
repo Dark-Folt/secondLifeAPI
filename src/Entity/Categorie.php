7c567a9ea96cf26bc3c4f64d6c26997ba919a39a
Dark-Folt <34268306+Dark-Folt@users.noreply.github.com>
2021-02-27 00:54:33 +0100

    ajout des groups dans l'entité categorie pour eviter les reference circulaires

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -3,10 +3,10 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
-use App\Repository\CategorieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource()
@@ -14,15 +14,18 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Categorie
 {
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"categorie:read"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=70)
+     * @Groups({"categorie:read"})
      */
     private $nom;
 
